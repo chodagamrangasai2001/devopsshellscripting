2a558ffbadeba5b217181725c2c0f76f28d8ebc6
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-11-17 16:49:54 +0530

    Tesing echo

--- echo.sh
+++ echo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Lets get vpc information for the region us-east-1 ......."
 aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r
-VPC_COUNT = $(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r | wc -l)
-echo $VPC_COUNT
+VPC_COUNT=$(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r | wc -l)
 echo ${VPC_COUNT}
 echo "${VPC_COUNT}"
 echo "Us-east-1 has a total vpc count of ${VPC_COUNT} Vpcs"
