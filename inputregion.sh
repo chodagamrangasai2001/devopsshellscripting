df442407215fcc81910be3b87e86de00c461ef9b
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-11-21 19:52:08 +0530

    debuging

--- inputregion.sh
+++ inputregion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 REGION=$1
-set -x
+set -x -e
 echo "Lets get vpc information for the region $REGION ......."
 aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r
 VPC_COUNT=$(aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r | wc -l)
