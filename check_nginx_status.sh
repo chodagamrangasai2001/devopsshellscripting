69ed3fa08d59a1c5b33f4ca9b21b44b0bbf322f8
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-11-22 13:27:31 +0530

    nginxstatus

---
+++ check_nginx_status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+systemctl status nginx
+if [$? -eq 0]; then
+    echo "nginx already running lets validate curl"
+    curl -sL http://localhost
+else
+    echo "nginx is not running lets start it"
+    curl -sL http://localhost
+fi
