8683afdc53276ab6616ea93f2876423bc0a1158a
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-11-25 15:14:49 +0530

    Testing_spl_args

---
+++ 9.Testing_special_args.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo $@
+echo $*
+if [$# -gt 0]; then
+    echo "you have provided $# Arguments to this script."
+    for REGION in $@; do
+        echo "Lets get vpc information for $REGION ......."
+        aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r
+        VPC_COUNT=$(aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r | wc -l)
+        echo "$REGION has a total vpc count of ${VPC_COUNT} Vpcs"
+    done
+else
+    REGION = $(aws ec2 describe-regions | jq ".Vpcs[].VpcId" -r)
+    echo "No Region arg provided"
+    echo $REGION
+fi
