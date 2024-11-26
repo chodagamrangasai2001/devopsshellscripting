af906bb0b9bb6cf2c35d50dd90af15104acde668
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-11-26 23:17:51 +0530

    even-odd.sh

--- 11.Find-even-odd.sh
+++ 11.Find-even-odd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-for I in {2..100}
-do 
-if [ $(expr $I % 2 ) == 0 ];
-then 
-echo "$I is the even number"
-fi
-done    
+rm -rf /tmp/EVEN.log && rm -rf /tmp/ODD.log
+for I in {2..100}; do
+    if [ $(expr $I % 2) == 0 ]; then
+        echo "$I is the even number" >>/tmp/EVEN.log
+    else
+        echo "$I is odd number" >>/tmp/ODD.log
+    fi
+done
