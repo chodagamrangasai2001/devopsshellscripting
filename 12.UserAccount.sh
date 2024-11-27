9c32be1acaebf8aec2daf4d2ce7b516af1a3a89b
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-11-27 23:46:52 +0530

    usercreation.sh

---
+++ 12.UserAccount.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while true; do
+    read -p "please enter username : " USERNAME
+    echo $USERNAME
+    EXISTING_USER=$(cat /etc/passwd | grep -i -w $USERNAME | cut -d ':' -f 1)
+    if [ "${EXISTING_USER}" = "${USERNAME}" ]; then
+        echo "The username $USERNAME already exists.. "
+    else
+        echo "User name $USERNAME is new user"
+        useradd -m $USERNAME --shell /bin/bash
+        SPEC = $(echo '!@#$%^&*' | fold -w1 | shuf | head -1)
+        PASSWORD = "India@${RANDOM}${SPEC}"
+        echo "$USERNAME : $PASSWORD" | sudo chpasswd
+        passwd -e $USERNAME
+        echo " Login with $USERNAME WITH PASSWORD $PASSWORD"
+    fi
+done
