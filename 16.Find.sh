e11e275a735aad504b6feaa05160a7abfda20e2c
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-02 23:57:06 +0530

    find.sh

--- 16.Find.sh
+++ 16.Find.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for FILE in $(find / -name awscliv2.zip); do
     echo $FILE
-    cp $FILE /tmp/demo
-    mv /tmp/demo/awscliv2.zip /tmp/demo/awscliv2-${RANDOM}.zip
+    cp $FILE /tmp/ranga
+    mv /tmp/ranga/awscliv2.zip /tmp/ranga/awscliv2-${RANDOM}.zip
 done
 ##############
 #!/bin/bash
