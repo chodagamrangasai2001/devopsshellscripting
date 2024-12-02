b5e39cb974b5b2302df1f2a830c0823838565197
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-02 10:56:43 +0530

    regex-validateurl

---
+++ 14.RegEX-validateURL.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DATA=$(cat applist.log)
+echo $DATA
+for URL in $DATA; do
+    if [[ "$URL" =~ ^https?://([^/]+) ]]; then
+        domain=${BASH_REMATCH[1]}
+        echo "$URL is a valid and domain name is $domain"
+    else
+        echo "$URL is a invalid and domain name is $domain"
+    fi
+done
