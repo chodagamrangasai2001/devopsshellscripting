52ad1a0d01ed17368237ae443f3e3b588f805388
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-02 11:00:51 +0530

    regex-validateurl

--- 14.RegEX-validateURL.sh
+++ 14.RegEX-validateURL.sh
@@ -3,9 +3,9 @@ DATA=$(cat applist.org)
 echo $DATA
 for URL in $DATA; do
     if [[ "$URL" =~ ^https?://([^/]+) ]]; then
-        domain=${BASH_REMATCH[1]}
-        echo "$URL is a valid and domain name is $domain"
-    else
-        echo "$URL is a invalid and domain name is $domain"
-    fi
+    domain=${BASH_REMATCH[1]}
+    echo "$URL is a valid and domain name is $domain"
+else
+    echo "$URL is invalid and domain name is not available"
+fi
 done
