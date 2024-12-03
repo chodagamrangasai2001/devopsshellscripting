f098951864e9ef2b26ba4a53b1812de623464171
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-03 12:37:22 +0530

    find.sh

--- 17.findwithdate.sh
+++ 17.findwithdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for FILE in $(find / -name awscliv2.zip); do
 echo $FILE
+mkdir -p /tmp/date 2> /dev/null
 cp $FILE /tmp/date
 DATE=$(date +%s)
 mv /tmp/date/awscliv2.zip /tmp/date/awscliv2-${DATE}.zip
