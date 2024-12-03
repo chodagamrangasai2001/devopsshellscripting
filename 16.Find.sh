3ab2d22dcd7cf21ff468889e3c64b01eafdbc603
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-03 12:11:48 +0530

    find.sh

--- 16.Find.sh
+++ 16.Find.sh
@@ -8,11 +8,12 @@ for FILE in $(find / -name awscliv2.zip); do
     mv /tmp/demo/awscliv2.zip /tmp/demo/awscliv2-${RANDOM}.zip
 done
 ##############
+
 #!/bin/bash
-##for FILE in $(find / -name awscliv2.zip); do
-#echo $FILE
-#cp $FILE /tmp/date
-#DATA=$(data +%s)
-#mv /tmp/date/awscliv2.zip /tmp/date/awscliv2-${DATE}.zip
-#ls -al /tmp/date
-#done
+for FILE in $(find / -name awscliv2.zip); do
+echo $FILE
+cp $FILE /tmp/date
+DATA=$(data +%s)
+mv /tmp/date/awscliv2.zip /tmp/date/awscliv2-${DATE}.zip
+ls -al /tmp/date
+done
