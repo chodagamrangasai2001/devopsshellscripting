e37120e733f5d1e925363062212ff8ff4431a04c
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-07 16:25:59 +0530

    functions.sh

--- 18.Functions_syntac.sh
+++ 18.Functions_syntac.sh
@@ -6,3 +6,16 @@ demofunc2() {
     echo "Welcom to demo func 2"
 }
 demofunc1
+demofunc2
+
+Get_VPC() {
+    echo "Running The Functon To List VPCs in $1"
+    vpc_list=$(aws ec2 describe-vpcs --region $1 | jq .Vpcs[].VpcId | tr -d '"')
+    for vpc in $(echo $vpc_list); do
+        echo "The VPC ID IS:$vpc"
+        echo "======================================="
+    done
+}
+for X in $@; do
+    Get_VPC $X
+done
