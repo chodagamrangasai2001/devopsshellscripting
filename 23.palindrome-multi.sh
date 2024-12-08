aa983ac6bc04b1bec49e6be9161eecbe163ebce6
Shanmukha V V Ranga Sai <shanmukha.veera@sifycorp.com>
2024-12-08 17:00:46 +0530

    Palindrome

---
+++ 23.palindrome-multi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for INPUT in $@;do
+SIZE=${#INPUT}
+STRING_LENGTH=$(expr $SIZE - 1)
+REVERSE=''
+#echo $STRING_LENGTH
+for ((I = $STRING_LENGTH; I >= 0; I--)); do
+    #echo ${INPUT[@]:$I:1}
+    REVERSE=${REVERSE}${INPUT[@]:$I:1}
+done
+if [ "${INPUT}" = "${REVERSE}" ]; then
+    echo "$INPUT is a palindrome"
+else
+    echo "$INPUT is not a palindrome"
+fi
+done
